63273aa3aca35c8412ab82d5d5f034fc27e8380b
hyperchessbot <hyperchessbot@gmail.com>
2021-02-07 08:09:35 +0100

    Parse body

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,14 @@
 exports.handler = async function(event, context) {
+	let blob = event.body
+
+	try{
+		blob = JSON.parse(event.body)
+	}catch(err){
+		console.log("could not parse body as json")
+	}
+	
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "Hello World", body: event.body})
+        body: "<pre>" + JSON.stringify({message: "Hello World", body: blob}, null, 2) + "</pre>"
     };
 }
